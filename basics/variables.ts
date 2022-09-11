ef2552627926e4840bd4059a9871c440559435cf
singhsidak54 <singhsidak54@gmail.com>
2022-09-12 01:55:16 +0530

    basic module completed

---
+++ basics/variables.ts
@@ -0,0 +1,11 @@
+let age = "eighteen";
+
+//not allowed because implicitly type is set to string
+// age = 18;
+
+let newAge: any = "eighteen";
+
+//alowed because type is set to any
+newAge = 18;
+
+//primitive types are - number, boolean, string
